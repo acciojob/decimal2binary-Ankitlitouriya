249f1920cdb675d25ec0d12ffc36292b38d24662
Ankit Litouriya <118179332+Ankitlitouriya@users.noreply.github.com>
2023-08-21 01:04:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function decimalToBinary(num2) {
+function decimalToBinary(num) {
   //Write you code here
 	//let  num =2;
-	num.toString(redix);
+	 //let b = decimal.toString( redix);
 //	console.log(num);
-	return decimalToBinary;
+	return num.toString(redix);
   
 }
 
+// const a = decimalToBinary(2);
+// console.log(2);
 
 window.decimalToBinary = decimalToBinary;
